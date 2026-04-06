7a048d81e5dc6ff6df1b6ba3caaaec5d5a501076
wooferzfg <spapushin@gmail.com>
2026-04-06 00:07:13 -0400

    Revert "Localize Subsplits runtime settings labels (#27)"
    This reverts commit 75a630820c985f9d293a6d9a5156dc6245a7e1d8.

--- src/LiveSplit.Subsplits/UI/Components/SplitsSettings.cs
+++ src/LiveSplit.Subsplits/UI/Components/SplitsSettings.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Windows.Forms;
 using System.Xml;
 
-using LiveSplit.Localization;
 using LiveSplit.Model;
 using LiveSplit.Model.Comparisons;
 using LiveSplit.TimeFormatters;
@@ -14,8 +13,6 @@ namespace LiveSplit.UI.Components;
 
 public partial class SplitsSettings : UserControl
 {
-    private static string T(string source) => UiLocalizer.Translate(source, LanguageResolver.ResolveCurrentCultureLanguage());
-
     private int _VisualSplitCount { get; set; }
     public int VisualSplitCount
     {
@@ -515,7 +512,7 @@ public partial class SplitsSettings : UserControl
             trkSize.Maximum = 120;
             SplitWidth = Math.Min(Math.Max(trkSize.Minimum, SplitWidth), trkSize.Maximum);
             trkSize.DataBindings.Add("Value", this, "SplitWidth", false, DataSourceUpdateMode.OnPropertyChanged);
-            lblSplitSize.Text = T("Split Width:");
+            lblSplitSize.Text = "Split Width:";
             chkDisplayRows.Enabled = false;
             chkDisplayRows.DataBindings.Clear();
             chkDisplayRows.Checked = true;
@@ -529,7 +526,7 @@ public partial class SplitsSettings : UserControl
             trkSize.Maximum = 250;
             ScaledSplitHeight = Math.Min(Math.Max(trkSize.Minimum, ScaledSplitHeight), trkSize.Maximum);
             trkSize.DataBindings.Add("Value", this, "ScaledSplitHeight", false, DataSourceUpdateMode.OnPropertyChanged);
-            lblSplitSize.Text = T("Split Height:");
+            lblSplitSize.Text = "Split Height:";
             chkDisplayRows.Enabled = true;
             chkDisplayRows.DataBindings.Clear();
             chkDisplayRows.DataBindings.Add("Checked", this, "Display2Rows", false, DataSourceUpdateMode.OnPropertyChanged);
